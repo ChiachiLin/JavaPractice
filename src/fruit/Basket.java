9f3452efee492331920822ced8326a07fd8011e3
JackyLin <jacky21807@gmail.com>
2018-11-26 20:40:34 +0800

    first commit

---
+++ src/fruit/Basket.java
@@ -0,0 +1,13 @@
+package fruit;
+
+import java.util.*;
+
+public class Basket<T>{
+	public T[] things;
+	public Basket(T...things){
+		this.things = things;
+		}
+	public void sort(Comparator<? super T>comparator) {
+		Arrays.sort(things,comparator);
+		}
+	}
